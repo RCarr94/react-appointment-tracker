f3737dbe638086b1fd3129827c1ab5566072a1fe
Ryan Carr <ryan.g.carr@hotmail.com>
2023-09-28 11:37:01 -0400

    inital commit, setup scheduler component

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ScheduleAppointment from '../../components/ScheduleAppointment';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Appointment Tracker</h1>
+      <ScheduleAppointment />
+    </div>
+  );
+}
+
+export default App;
